9e58b2b2c855448a7b13eb6b3d4e0d3fa981685b
Anthony <bonsilaoanthony@gmail.com>
2022-09-27 15:07:18 -0400

    index/home page design

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["design/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        cyranprocess: '#5BC0EB',
+        minionyellow: '#FDE74C',
+        androidgreen: '#9BC53D',
+        flame: '#E55934',
+        pumpkin: '#FA7921'
+      }
+    },
+  },
+  plugins: [],
+}
